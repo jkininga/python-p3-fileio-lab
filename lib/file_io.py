efb19d4790e35deb7a68960ceb74487d2dabc856
Caroline Ininga <carolineininga@gmail.com>
2025-07-04 09:54:00 +0300

    completed file I/O lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,27 @@
 def write_file(file_name, file_content):
-    pass
+    enhanced = str(file_name) + '.txt'
+    with open(enhanced, mode = 'w', encoding='utf-8') as f:
+        f.write(file_content)
+   
 
-def append_file(file_name, append_content):
-    pass
 
+def append_file(file_name, file_content):
+    enhanced = str(file_name) + '.txt'
+    with open(enhanced, mode = 'a', encoding='utf-8') as f:
+        f.write(file_content)
+ 
+    
+  
 def read_file(file_name):
-    pass
+    enhanced = str(file_name) + '.txt'
+    with open( enhanced, encoding= 'utf-8') as f:
+        return f.read()
+
+
+
+
+
+write_file("text", "This is test content")
+append_file('text', "This is appended content")
+print(read_file('text'))
+
